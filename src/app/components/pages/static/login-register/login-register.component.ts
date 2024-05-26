f66ac553e0e70b5db56866fe2d99984bb30753e4
DanielRodr1 <drodrigueza10@upao.edu.pe>
2024-05-26 18:56:09 -0500

    Add Code Seller in the form of Register

--- src/app/components/pages/static/login-register/login-register.component.ts
+++ src/app/components/pages/static/login-register/login-register.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login-register',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './login-register.component.html',
   styleUrl: './login-register.component.css'
 })
 export class LoginRegisterComponent {
 
+  public codigo : boolean;
+
+  public constructor() {
+    this.codigo = false;
+  }
 
 
+  onRoleChange(event: Event) : void {
+    const roleSelect = event.target as HTMLSelectElement; //Captura el Select con su value
+    const value = roleSelect.value; //Agarra el value del Select
+
+    if(value == 'vendedor'){
+      this.codigo = true;
+    } else {
+      this.codigo = false
+    }
+  }
 }
