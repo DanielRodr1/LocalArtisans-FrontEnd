1f45566b17c5eee376f2901c148e3cb83e7ebc1b
21MarcoBM <bmmarco056@gmail.com>
2024-06-01 13:54:33 -0500

    ListProducts and PublishProducts

---
+++ src/app/components/pages/static/publish-products/publish-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publish-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './publish-products.component.html',
+  styleUrl: './publish-products.component.css'
+})
+export class PublishProductsComponent {
+
+}
