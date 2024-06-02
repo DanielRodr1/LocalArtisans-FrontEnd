4d18d01ada48ffb802eda40f52b57fc5f40d58c9
DanielRodr1 <drodrigueza10@upao.edu.pe>
2024-06-02 11:41:20 -0500

    VALIDATION OF HEADER JUST FOR HOME-PAGE

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavComponent} from "./nav/nav.component";
+import {NgIf} from "@angular/common";
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NavComponent
+    NavComponent,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  showCarousel: boolean;
 
+  constructor(private router: Router) {
+    this.showCarousel = false;
+  }
+
+  ngOnInit() {
+    // Escuchar los eventos de navegación del router
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Verificar si la ruta actual es 'home-page'
+      this.showCarousel = event.urlAfterRedirects === '/home-page';
+    });
+  }
 }
