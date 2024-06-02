0e4ea15a69456e0a2c1b1951d035385a46e153d1
CRISTHIAN ENRIQUE CUEVA WONG <ccuevaw1@upao.edu.pe>
2024-06-02 10:50:38 -0500

    INTERFACE: Home_vendedor/ PerfilCliente

---
+++ src/app/components/pages/static/home-vendedor/home-vendedor.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-vendedor',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-vendedor.component.html',
+  styleUrl: './home-vendedor.component.css'
+})
+export class HomeVendedorComponent {
+
+  selectCategory(ceramica: string) {
+    
+  }
+}
---
+++ src/app/components/pages/static/perfil-cliente/perfil-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-cliente.component.html',
+  styleUrl: './perfil-cliente.component.css'
+})
+export class PerfilClienteComponent implements OnInit{
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
