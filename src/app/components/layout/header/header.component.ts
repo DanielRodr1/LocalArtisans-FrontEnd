24b01eb9cd7a2b5802d777abb41561113b38f6f2
harrycas <hcastror1@upao.edu.pe>
2024-06-09 18:39:39 -0500

    Artisans code

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -14,6 +14,7 @@ import {filter} from "rxjs";
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent implements OnInit{
   showCarousel: boolean;
 
